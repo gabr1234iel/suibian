2e7e972ab19859006db9c9e983537901e0ffdbf1
jiayi <jiayi031209@gmail.com>
2025-08-10 07:37:19 +0800

    Update tab logo

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import { AppContextProvider } from '../context/AppContext';
 import '../styles/globals.css';
 import { useEffect } from 'react';
@@ -19,11 +20,21 @@ function MyApp({ Component, pageProps }: AppProps) {
   }, []);
 
   return (
-    <div className={jetbrainsMono.className}>
-      <AppContextProvider>
-          <Component {...pageProps} />
-      </AppContextProvider>
-    </div>
+    <>
+      <Head>
+        <title>SuiBian - Decentralized Trading Agents</title>
+        <meta name="description" content="SuiBian is a decentralized platform for creating, deploying, and subscribing to autonomous trading agents on the Sui blockchain." />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.png" />
+        <link rel="shortcut icon" href="/favicon.png" />
+        <link rel="apple-touch-icon" href="/favicon.png" />
+      </Head>
+      <div className={jetbrainsMono.className}>
+        <AppContextProvider>
+            <Component {...pageProps} />
+        </AppContextProvider>
+      </div>
+    </>
   );
 }
 
